54fb1bfa3fb7b66a1ad3f1eb414dc86e7aff995b
Remi THEBAULT <remi.thebault@gmail.com>
2017-02-08 00:23:00 +0100

    client: WlProxy is stored on user data

--- client/source/wayland/client/core.d
+++ client/source/wayland/client/core.d
@@ -120,7 +120,8 @@ abstract class WlDisplayBase : WlProxy, Native!wl_display
     }
 }
 
-
+// WlProxy wrap objects living in the C API. For fast retrieval, we use the
+// user data offered by wayland.
 abstract class WlProxy
 {
     private wl_proxy* _proxy;
@@ -128,6 +129,30 @@ abstract class WlProxy
     protected this(wl_proxy* proxy)
     {
         _proxy = proxy;
+
+        import core.memory : GC;
+        GC.addRoot(cast(void*)this);
+        GC.setAttr(cast(void*)this, GC.BlkAttr.NO_MOVE);
+
+        wl_proxy_set_user_data(_proxy, cast(void*)this);
+    }
+
+    protected void destroyNotify()
+    {
+        wl_proxy_set_user_data(_proxy, null);
+
+        import core.memory : GC;
+
+        GC.removeRoot(cast(void*)this);
+        GC.clrAttr(cast(void*)this, GC.BlkAttr.NO_MOVE);
+    }
+
+    static WlProxy get(wl_proxy* proxy)
+    {
+        if (!proxy) return null;
+        auto ud = wl_proxy_get_user_data(proxy);
+        if (!ud) return null;
+        else return cast(WlProxy)ud;
     }
 
     protected final @property inout(wl_proxy)* proxy() inout
