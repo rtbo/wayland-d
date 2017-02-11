86673541a3da75786a275f7116fbfc64c55f508f
Remi THEBAULT <remi.thebault@gmail.com>
2017-02-11 19:01:41 +0100

    examples: list_registry

---
+++ examples/list_registry/source/list_registry.d
@@ -0,0 +1,20 @@
+// Copyright © 2017 Rémi Thebault
+module list_registry;
+
+import wayland.client;
+import std.exception;
+import std.stdio;
+
+void main()
+{
+    auto display = enforce(WlDisplay.connect());
+	scope(exit) display.disconnect();
+
+    auto reg = enforce(display.getRegistry());
+	scope(exit) reg.destroy();
+
+    reg.onGlobal = (WlRegistry /+reg+/, uint /+name+/, string iface, uint /+ver+/) {
+        writeln("registering ", iface);
+    };
+    display.roundtrip();
+}
