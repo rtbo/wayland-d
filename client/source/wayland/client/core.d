b15c475ac09f96c17bb52fb660ed826c6650ee2d
Remi THEBAULT <remi.thebault@gmail.com>
2017-02-11 10:41:49 +0100

    client: proxy lookup done with AA
    Previously was user_data, which for some reason
    didn't work. AA has advantage that GC naturally
    keeps track of the living objects.

--- client/source/wayland/client/core.d
+++ client/source/wayland/client/core.d
@@ -126,33 +126,24 @@ abstract class WlProxy
 {
     private wl_proxy* _proxy;
 
+    private static WlProxy[wl_proxy*] proxyCache;
+
     protected this(wl_proxy* proxy)
     {
         _proxy = proxy;
-
-        import core.memory : GC;
-        GC.addRoot(cast(void*)this);
-        GC.setAttr(cast(void*)this, GC.BlkAttr.NO_MOVE);
-
-        wl_proxy_set_user_data(_proxy, cast(void*)this);
+        proxyCache[proxy] = this;
     }
 
     protected void destroyNotify()
     {
-        wl_proxy_set_user_data(_proxy, null);
-
-        import core.memory : GC;
-
-        GC.removeRoot(cast(void*)this);
-        GC.clrAttr(cast(void*)this, GC.BlkAttr.NO_MOVE);
+        proxyCache.remove(_proxy);
     }
 
     static WlProxy get(wl_proxy* proxy)
     {
-        if (!proxy) return null;
-        auto ud = wl_proxy_get_user_data(proxy);
-        if (!ud) return null;
-        else return cast(WlProxy)ud;
+        auto pp = proxy in proxyCache;
+        if (pp) return *pp;
+        else return null;
     }
 
     protected final @property inout(wl_proxy)* proxy() inout
