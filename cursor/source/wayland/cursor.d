d9e0fd2093e6d321853eb94f5b1ebc18806e55d5
Remi THEBAULT <remi.thebault@gmail.com>
2017-02-16 20:37:08 +0100

    wayland-cursor bindings

---
+++ cursor/source/wayland/cursor.d
@@ -0,0 +1,157 @@
+module wayland.cursor;
+
+import wayland.native.cursor;
+import wayland.client;
+import wayland.util;
+
+import std.string;
+
+
+class WlCursorImage : Native!wl_cursor_image
+{
+    mixin nativeImpl!(wl_cursor_image);
+
+    this(wl_cursor_image* native)
+    {
+        _native = native;
+    }
+
+    @property uint width()
+    {
+        return native.width;
+    }
+
+    @property uint height()
+    {
+        return native.height;
+    }
+
+    @property uint hotspotX()
+    {
+        return native.hotspot_x;
+    }
+
+    @property uint hotspotY()
+    {
+        return native.hotspot_y;
+    }
+
+    @property uint delay()
+    {
+        return native.delay;
+    }
+
+    @property WlBuffer buffer()
+    {
+        auto nb = wl_cursor_image_get_buffer(native);
+        if (!nb) return null;
+        auto buf = cast(WlBuffer)WlProxy.get(nb);
+        if (!buf) buf = cast(WlBuffer)WlBuffer.iface.makeProxy(nb);
+        return buf;
+    }
+}
+
+class WlCursor : Native!wl_cursor
+{
+    mixin nativeImpl!(wl_cursor);
+
+    private WlCursorImage[] _images;
+
+    this(wl_cursor* native)
+    {
+        _native = native;
+    }
+
+    @property string name()
+    {
+        return fromStringz(native.name).idup;
+    }
+
+    @property WlCursorImage[] images()
+    {
+        import std.array : uninitializedArray;
+
+        if (_images) return _images;
+
+        _images = uninitializedArray!(WlCursorImage[])(native.image_count);
+        foreach (i; 0 .. native.image_count)
+        {
+            _images[i] = new WlCursorImage(native.images[i]);
+        }
+        return _images;
+    }
+
+    int frame(uint time)
+    {
+        return wl_cursor_frame(native, time);
+    }
+
+    int frameAndDuration(uint time, out uint duration)
+    {
+        return wl_cursor_frame_and_duration(native, time, &duration);
+    }
+}
+
+class WlCursorTheme : Native!wl_cursor_theme
+{
+    mixin nativeImpl!(wl_cursor_theme);
+
+    this(wl_cursor_theme* native)
+    {
+        _native = native;
+    }
+
+    static WlCursorTheme load(string name, size_t size, WlShm shm)
+    {
+        auto ct = wl_cursor_theme_load(toStringz(name), cast(int)size, shm.proxy);
+        return ct ? new WlCursorTheme(ct) : null;
+    }
+
+    void destroy()
+    {
+        wl_cursor_theme_destroy(native);
+    }
+
+    WlCursor cursor(string name)
+    {
+        auto cp = wl_cursor_theme_get_cursor(native, toStringz(name));
+        return cp ? new WlCursor(cp) : null;
+    }
+
+}
+
+version(WlDynamic)
+{
+    import derelict.util.loader : SharedLibLoader;
+
+    private class WlCursorLoader : SharedLibLoader
+    {
+        this()
+        {
+            super("libwayland-cursor.so");
+        }
+
+        protected override void loadSymbols()
+        {
+            bindFunc( cast( void** )&wl_cursor_theme_load, "wl_cursor_theme_load" );
+            bindFunc( cast( void** )&wl_cursor_theme_destroy, "wl_cursor_theme_destroy" );
+            bindFunc( cast( void** )&wl_cursor_theme_get_cursor, "wl_cursor_theme_get_cursor" );
+            bindFunc( cast( void** )&wl_cursor_image_get_buffer, "wl_cursor_image_get_buffer" );
+            bindFunc( cast( void** )&wl_cursor_frame, "wl_cursor_frame" );
+            bindFunc( cast( void** )&wl_cursor_frame_and_duration, "wl_cursor_frame_and_duration" );
+        }
+    }
+
+    private __gshared WlCursorLoader _loader;
+
+    shared static this()
+    {
+        _loader = new WlCursorLoader;
+    }
+
+    public @property SharedLibLoader wlCursorDynLib()
+    {
+        return _loader;
+    }
+}
+
---
+++ cursor/source/wayland/native/cursor.d
@@ -0,0 +1,101 @@
+// Copyright © 2017 Rémi Thebault
+/// bindings to wayland-client-core.h
+module wayland.native.cursor;
+
+// Wayland client-core copyright:
+/*
+ * Copyright © 2012 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the
+ * next paragraph) shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import wayland.native.client : wl_proxy;
+
+extern(C)
+{
+    struct wl_cursor_theme;
+
+    struct wl_cursor_image {
+        uint width;		/* actual width */
+        uint height;	/* actual height */
+        uint hotspot_x;	/* hot spot x (must be inside image) */
+        uint hotspot_y;	/* hot spot y (must be inside image) */
+        uint delay;		/* animation delay to next frame (ms) */
+    };
+
+    struct wl_cursor {
+        uint image_count;
+        wl_cursor_image** images;
+        char* name;
+    };
+}
+
+version(WlDynamic)
+{
+    extern(C) nothrow
+    {
+        alias da_wl_cursor_theme_load = wl_cursor_theme* function (const(char)* name, int size, wl_proxy* shm);
+
+        alias da_wl_cursor_theme_destroy = void function (wl_cursor_theme* theme);
+
+        alias da_wl_cursor_theme_get_cursor = wl_cursor* function (wl_cursor_theme* theme, const(char)* name);
+
+        alias da_wl_cursor_image_get_buffer = wl_proxy* function (wl_cursor_image* image);
+
+        alias da_wl_cursor_frame = int function (wl_cursor* cursor, uint time);
+
+        alias da_wl_cursor_frame_and_duration = int function (wl_cursor* cursor, uint time, uint* duration);
+    }
+
+    __gshared
+    {
+        da_wl_cursor_theme_load wl_cursor_theme_load;
+
+        da_wl_cursor_theme_destroy wl_cursor_theme_destroy;
+
+        da_wl_cursor_theme_get_cursor wl_cursor_theme_get_cursor;
+
+        da_wl_cursor_image_get_buffer wl_cursor_image_get_buffer;
+
+        da_wl_cursor_frame wl_cursor_frame;
+
+        da_wl_cursor_frame_and_duration wl_cursor_frame_and_duration;
+    }
+}
+
+version(WlStatic)
+{
+    extern(C) nothrow
+    {
+        wl_cursor_theme* wl_cursor_theme_load(const(char)* name, int size, wl_proxy* shm);
+
+        void wl_cursor_theme_destroy(wl_cursor_theme* theme);
+
+        wl_cursor* wl_cursor_theme_get_cursor(wl_cursor_theme* theme, const(char)* name);
+
+        wl_proxy* wl_cursor_image_get_buffer(wl_cursor_image* image);
+
+        int wl_cursor_frame(wl_cursor* cursor, uint time);
+
+        int wl_cursor_frame_and_duration(wl_cursor* cursor, uint time, uint* duration);
+    }
+}
