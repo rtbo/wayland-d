214bf761ff4c2bc1bc7f44908106674638ca6ad2
Remi THEBAULT <remi.thebault@gmail.com>
2017-02-18 09:17:02 +0100

    example: egl_window use xdg-shell

--- examples/egl_window/source/egl_window.d
+++ examples/egl_window/source/egl_window.d
@@ -5,6 +5,10 @@ module egl_window;
 
 import wayland.client;
 import wayland.egl;
+import wayland.cursor;
+import wayland.util;
+import wayland.native.util;
+import zxdg_shell_v6;
 import derelict.gles.egl;
 import derelict.gles.gles2;
 
@@ -21,20 +25,64 @@ class Display
 {
 	WlDisplay display;
 	WlCompositor compositor;
-	WlShell shell;
+	WlShm shm;
+	WlSeat seat;
+	ZxdgShellV6 shell;
+	WlCursorTheme cursorTheme;
+	WlCursor defaultCursor;
+	WlSurface cursorSurf;
+	WlPointer pointer;
+	WlKeyboard kbd;
+
 	EGLDisplay eglDisplay;
 	EGLContext eglContext;
 	EGLConfig config;
 
+	EglWindow window;
+
 	this()
 	{
-		DerelictEGL.load();
-
 		display = enforce(WlDisplay.connect());
 		auto reg = display.getRegistry();
-		reg.onGlobal = &regGlobal;
+		reg.onGlobal = (WlRegistry reg, uint name, string iface, uint ver)
+		{
+			import std.algorithm : min;
+			if (iface == WlCompositor.iface.name)
+			{
+				compositor = cast(WlCompositor)reg.bind(name, WlCompositor.iface, min(ver, 4));
+			}
+			else if (iface == WlShm.iface.name)
+			{
+				shm = cast(WlShm)reg.bind(name, WlShm.iface, min(ver, 1));
+				cursorTheme = enforce(
+					WlCursorTheme.load(null, 32, shm),
+					"Unable to load default cursor theme"
+				);
+				defaultCursor = enforce(
+					cursorTheme.cursor("left_ptr"),
+					"Unable to load default left pointer"
+				);
+			}
+			else if (iface == WlSeat.iface.name)
+			{
+				seat = cast(WlSeat)reg.bind(name, WlSeat.iface, min(ver, 1));
+				seat.onCapabilities = &seatCapChanged;
+
+			}
+			else if (iface == ZxdgShellV6.iface.name)
+			{
+				shell = cast(ZxdgShellV6)reg.bind(name, ZxdgShellV6.iface, min(ver, 1));
+				shell.onPing = (ZxdgShellV6 shell, uint serial) {
+					shell.pong(serial);
+				};
+			}
+		};
 		display.roundtrip();
 		reg.destroy();
+
+		cursorSurf = compositor.createSurface();
+
+		DerelictEGL.load();
 
 		eglDisplay = enforce(eglGetDisplay (cast(void*)display.proxy));
 		enforce(eglInitialize(eglDisplay, null, null) == EGL_TRUE);
@@ -60,17 +108,69 @@ class Display
 		eglContext = enforce(eglCreateContext (eglDisplay, config, EGL_NO_CONTEXT, ctxAttribs.ptr));
 	}
 
-	void regGlobal(WlRegistry reg, uint name, string iface, uint ver)
+
+	void seatCapChanged (WlSeat seat, WlSeat.Capability cap)
 	{
-		import std.algorithm : min;
-		if (iface == WlCompositor.iface.name)
+		if ((cap & WlSeat.Capability.pointer) && !pointer)
 		{
-			compositor = cast(WlCompositor)reg.bind(name, WlCompositor.iface, min(ver, 4));
+			pointer = seat.getPointer();
+			pointer.onEnter = &pointerEnter;
+			pointer.onButton = &pointerButton;
 		}
-		else if (iface == WlShell.iface.name)
+		else if (!(cap & WlSeat.Capability.pointer) && pointer)
 		{
-			shell = cast(WlShell)reg.bind(name, WlShell.iface, min(ver, 1));
+			pointer.destroy();
+			pointer = null;
 		}
+
+		if ((cap & WlSeat.Capability.keyboard) && !kbd)
+		{
+			kbd = seat.getKeyboard();
+			kbd.onKey = &kbdKey;
+		}
+		else if (!(cap & WlSeat.Capability.keyboard) && kbd)
+		{
+			kbd.destroy();
+			kbd = null;
+		}
+	}
+
+	void pointerEnter(WlPointer pointer, uint serial, WlSurface surface,
+			WlFixed sx, WlFixed sy)
+	{
+		if (defaultCursor)
+		{
+			auto img = defaultCursor.images[0];
+			auto buf = img.buffer;
+			if (!buf) return;
+			pointer.setCursor(serial, cursorSurf, img.hotspotX, img.hotspotY);
+			cursorSurf.attach(buf, 0, 0);
+			cursorSurf.damage(0, 0, img.width, img.height);
+			cursorSurf.commit();
+		}
+	}
+
+	void pointerButton(WlPointer pointer, uint serial, uint time, uint button,
+			WlPointer.ButtonState state)
+	{
+		import linux.input : BTN_LEFT;
+
+		if (!window || !window.topLevel) return;
+
+		if (button == BTN_LEFT && state == WlPointer.ButtonState.pressed)
+		{
+			window.topLevel.move(seat, serial);
+		}
+	}
+
+	void kbdKey(WlKeyboard keyboard, uint serial, uint time, uint key,
+			WlKeyboard.KeyState state)
+	{
+		import linux.input : KEY_ESC;
+
+		if (!window) return;
+
+		if (key == KEY_ESC && state) window.running = false;
 	}
 
 	void destroy()
@@ -87,7 +187,9 @@ class EglWindow
 	Display dpy;
 
 	WlSurface surf;
-	WlShellSurface shSurf;
+	ZxdgSurfaceV6 xdgSurf;
+	ZxdgToplevelV6 topLevel;
+
 	WlEglWindow eglWin;
 	EGLSurface eglSurf;
 
@@ -104,23 +206,23 @@ class EglWindow
 	this (Display dpy)
 	{
 		this.dpy = dpy;
+
 		surf = enforce(dpy.compositor.createSurface());
-		shSurf = enforce(dpy.shell.getShellSurface(surf));
-		shSurf.setToplevel();
+		xdgSurf = enforce(dpy.shell.getXdgSurface(surf));
+		topLevel = enforce(xdgSurf.getToplevel());
+
+		topLevel.onConfigure = &onTLConfigure;
+		topLevel.onClose = &onTLClose;
+		topLevel.setTitle("wayland-d - EGL window");
+
+		xdgSurf.onConfigure = (ZxdgSurfaceV6 surf, uint serial)
+		{
+			surf.ackConfigure(serial);
+			configured = true;
+		};
 
 		eglWin = new WlEglWindow(surf, winWidth, winHeight);
 		eglSurf = enforce(eglCreateWindowSurface(dpy.eglDisplay, dpy.config, cast(void*)eglWin.native, null));
-
-		shSurf.onPing = (WlShellSurface surf, uint serial) {
-			surf.pong(serial);
-		};
-		shSurf.onConfigure = (WlShellSurface surf,
-								WlShellSurface.Resize edges,
-								int width, int height)
-		{
-			eglWin.resize(width, height, 100, 100);
-			configured = true;
-		};
 
 		enforce(eglMakeCurrent (dpy.eglDisplay, eglSurf, eglSurf, dpy.eglContext) == GL_TRUE);
 
@@ -130,6 +232,17 @@ class EglWindow
 
 		initGl();
 		startTime = MonoTime.currTime();
+		surf.commit();
+	}
+
+	void onTLConfigure(ZxdgToplevelV6, int width, int height, wl_array* states)
+	{
+		if (eglWin) eglWin.resize(winWidth, winHeight, 100, 100);
+	}
+
+	void onTLClose(ZxdgToplevelV6)
+	{
+		running = false;
 	}
 
 	GLuint createShader(string source, GLenum stage)
@@ -233,7 +346,6 @@ class EglWindow
 		immutable speedDiv = 5f;
 		immutable msecs = (MonoTime.currTime - startTime).total!"msecs";
 		immutable angle = ((msecs / speedDiv) % 360) * PI / 180f;
-		if (msecs >= 5000) running = false;
 
 		immutable s = sin(angle);
 		immutable c = cos(angle);
@@ -267,7 +379,8 @@ class EglWindow
 		glDeleteBuffers(1, &vbo);
 		eglDestroySurface(dpy.eglDisplay, eglSurf);
 		eglWin.destroy();
-		shSurf.destroy();
+		topLevel.destroy();
+		xdgSurf.destroy();
 		surf.destroy();
 	}
 }
@@ -296,6 +409,8 @@ int main ()
 	scope(exit) dpy.destroy();
 	auto win = new EglWindow(dpy);
 	scope(exit) win.destroy();
+
+	dpy.window = win;
 
 	while(win.running)
 	{
