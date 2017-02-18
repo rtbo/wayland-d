29a27d7be334a073c7c2232e8ed2429c4e9f8c04
Remi THEBAULT <remi.thebault@gmail.com>
2017-02-18 09:38:13 +0100

    gen protocol only if needed

--- client/gen_protocol.sh
+++ client/gen_protocol.sh
@@ -3,9 +3,19 @@
 CLIENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 WL_D_DIR=$CLIENT_DIR/..
 
+PROTOCOL=$WL_D_DIR/protocol/wayland.xml
+CLIENT_SRC=$CLIENT_DIR/source/wayland/client/protocol.d
+SCANNER_SRC=$WL_D_DIR/scanner/source/wayland/scanner.d
+
+if [ $PROTOCOL -ot $CLIENT_SRC ]; then
+    if [ $SCANNER_SRC -ot $CLIENT_SRC ]; then
+        exit 0
+    fi
+fi
+
 cd $WL_D_DIR
 
 dub run wayland-d:scanner -- \
                 -m wayland.client.protocol \
-                -i protocol/wayland.xml \
-                -o client/source/wayland/client/protocol.d
+                -i $PROTOCOL \
+                -o $CLIENT_SRC
