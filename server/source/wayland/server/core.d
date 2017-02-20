af0701ec84f584169f3a4ee07799b5be075a9d62
Remi THEBAULT <remi.thebault@gmail.com>
2017-02-20 13:11:43 +0100

    server: completing WlClient and WlResource

--- server/source/wayland/server/core.d
+++ server/source/wayland/server/core.d
@@ -5,6 +5,8 @@ import wayland.native.server;
 import wayland.util;
 
 import std.string;
+import std.exception : enforce;
+import core.sys.posix.sys.types;
 
 
 class WlDisplayBase : Native!wl_display
@@ -17,8 +19,8 @@ class WlDisplayBase : Native!wl_display
     // one loop per display, so no need to use an object store
     private WlEventLoop _loop;
 
-    private DestroyDg _destroyDg;
-    private ClientCreatedDg _clientCreatedDg;
+    private DestroyDg _onDestroy;
+    private ClientCreatedDg _onClientCreated;
 
     private WlClient[] _clients;
 
@@ -47,18 +49,18 @@ class WlDisplayBase : Native!wl_display
         wl_display_destroy(native);
         assert(!ObjectCache.get(native));
         _native = null;
-        _destroyDg = null;
-        _clientCreatedDg = null;
+        _onDestroy = null;
+        _onClientCreated = null;
     }
 
     @property void onDestroy(DestroyDg dg)
     {
-        _destroyDg = dg;
+        _onDestroy = dg;
     }
 
     @property void onClientCreated(ClientCreatedDg dg)
     {
-        _clientCreatedDg = dg;
+        _onClientCreated = dg;
     }
 
     @property WlEventLoop eventLoop()
@@ -109,7 +111,7 @@ class WlDisplayBase : Native!wl_display
 
     @property void destroyListener(DestroyDg dg)
     {
-        _destroyDg = dg;
+        _onDestroy = dg;
     }
 
     WlClient createClient(int fd)
@@ -130,7 +132,7 @@ class WlDisplayBase : Native!wl_display
         auto cl = new WlClient(natCl);
         ObjectCache.set(natCl, cl);
         _clients ~= cl;
-        if (_clientCreatedDg) _clientCreatedDg(cl);
+        if (_onClientCreated) _onClientCreated(cl);
     }
 
     private void unregisterClient(wl_client* natCl)
@@ -140,6 +142,8 @@ class WlDisplayBase : Native!wl_display
         assert(cl, "could not retrieve client from obj cache");
         if (cl._onDestroy) cl._onDestroy(cl);
         _clients = _clients.remove!(c => c is cl);
+        cl._onDestroy = null;
+        cl._native = null;
         ObjectCache.remove(natCl);
     }
 }
@@ -158,7 +162,7 @@ class WlEventLoop : Native!wl_event_loop
         alias IdleDg = void delegate ();
     }
 
-    private DestroyDg _destroyDg;
+    private DestroyDg _onDestroy;
 
     this (wl_event_loop* native)
     {
@@ -176,13 +180,11 @@ class WlEventLoop : Native!wl_event_loop
     {
         wl_event_loop_destroy(_native);
         assert(!ObjectCache.get(native));
-        _destroyDg = null;
-        _native = null;
     }
 
     @property void destroyListener(DestroyDg dg)
     {
-        _destroyDg = dg;
+        _onDestroy = dg;
     }
 
     @property int fd()
@@ -319,6 +321,13 @@ class WlIdleEventSource : WlEventSource
     }
 }
 
+struct Credentials
+{
+    pid_t pid;
+    uid_t uid;
+    gid_t gid;
+}
+
 class WlClient : Native!wl_client
 {
     mixin nativeImpl!wl_client;
@@ -328,6 +337,7 @@ class WlClient : Native!wl_client
 
     DestroyDg _onDestroy;
     ResourceCreatedDg _onResourceCreated;
+    WlResource[] _resources;
 
     this (wl_client* native)
     {
@@ -344,6 +354,63 @@ class WlClient : Native!wl_client
         _onResourceCreated = null;
         _native = null;
     }
+
+    void flush()
+    {
+        wl_client_flush(native);
+    }
+
+    @property Credentials credentials()
+    {
+        Credentials res;
+        wl_client_get_credentials(native, &res.pid, &res.uid, &res.gid);
+        return res;
+    }
+
+    @property int fd()
+    {
+        return wl_client_get_fd(native);
+    }
+
+    WlResource object(uint id)
+    {
+        auto natRes = wl_client_get_object(native, id);
+        if (!natRes) return null;
+        auto res = cast(WlResource)ObjectCache.get(natRes);
+        return res ? res : new WlResource(natRes);
+    }
+
+    void postNoMemory()
+    {
+        wl_client_post_no_memory(native);
+    }
+
+    @property WlDisplayBase display()
+    {
+        auto natDpy = wl_client_get_display(native);
+        assert(natDpy);
+        return enforce(cast(WlDisplayBase)ObjectCache.get(natDpy));
+    }
+
+    private void registerNewResource(wl_resource* natRes)
+    {
+        auto res = new WlResource(natRes);
+        ObjectCache.set(natRes, res);
+        _resources ~= res;
+        if (_onResourceCreated) _onResourceCreated(res);
+    }
+
+    private void unregisterResource(wl_resource* natRes)
+    {
+        import std.algorithm : remove;
+        WlResource res = cast(WlResource)ObjectCache.get(natRes);
+        assert(res, "could not retrieve resource from obj cache");
+        if (res._onDestroy) res._onDestroy(res);
+        _resources = _resources.remove!(r => r is res);
+        res._onDestroy = null;
+        res._native = null;
+        ObjectCache.remove(natRes);
+    }
 }
 
 class WlResource : Native!wl_resource
@@ -356,18 +423,37 @@ class WlResource : Native!wl_resource
     this (wl_resource* native)
     {
         _native = native;
-        ObjectCache.set(native, this);
         wl_resource_add_destroy_listener(native, &resourceDestroyListener);
     }
 
     void destroy()
     {
         wl_resource_destroy(native);
-        ObjectCache.remove(native);
-        _onDestroy = null;
-        _native = null;
+    }
+
+    @property uint id()
+    {
+        return wl_resource_get_id(native);
+    }
+
+    @property WlClient client()
+    {
+        auto natCl = wl_resource_get_client(native);
+        assert(natCl);
+        return enforce(cast(WlClient)ObjectCache.get(natCl));
+    }
+
+    @property int ver()
+    {
+        return wl_resource_get_version(native);
+    }
+
+    @property string cls()
+    {
+        return fromStringz(wl_resource_get_class(native)).idup;
     }
 }
+
 
 private extern(C) nothrow
 {
@@ -376,7 +462,7 @@ private extern(C) nothrow
         nothrowFnWrapper!({
             auto dpy = cast(WlDisplayBase)ObjectCache.get(data);
             assert(dpy);
-            if (dpy._destroyDg) dpy._destroyDg();
+            if (dpy._onDestroy) dpy._onDestroy();
             ObjectCache.remove(data);
         });
     }
@@ -386,7 +472,7 @@ private extern(C) nothrow
         nothrowFnWrapper!({
             auto el = cast(WlEventLoop)ObjectCache.get(data);
             assert(el);
-            if (el._destroyDg) el._destroyDg(el);
+            if (el._onDestroy) el._onDestroy(el);
             ObjectCache.remove(data);
         });
     }
@@ -402,7 +488,7 @@ private extern(C) nothrow
         });
     }
 
-    void clientDestroy(wl_listener* listener, void* data)
+    void clientDestroy(wl_listener*, void* data)
     {
         nothrowFnWrapper!({
             auto natCl = cast(wl_client*)data;
@@ -413,14 +499,26 @@ private extern(C) nothrow
         });
     }
 
-    void resourceCreated(wl_listener* listener, void* data)
+    void resourceCreated(wl_listener*, void* data)
     {
-
+        nothrowFnWrapper!({
+            auto natRes = cast(wl_resource*)data;
+            auto natCl = wl_resource_get_client(natRes);
+            auto cl = cast(WlClient)ObjectCache.get(natCl);
+            assert(cl, "could not retrieve client from obj cache");
+            cl.registerNewResource(natRes);
+        });
     }
 
-    void resourceDestroy(wl_listener* listener, void* data)
+    void resourceDestroy(wl_listener*, void* data)
     {
-
+        nothrowFnWrapper!({
+            auto natRes = cast(wl_resource*)data;
+            auto natCl = wl_resource_get_client(natRes);
+            auto cl = cast(WlClient)ObjectCache.get(natCl);
+            assert(cl, "could not retrieve client from obj cache");
+            cl.unregisterResource(natRes);
+        });
     }
 
 
